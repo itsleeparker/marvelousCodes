d0e2eb4ba3eb218a8f305b16802bd1bfb2097d69
lee parker <leesan@pop-os.localdomain>
2022-05-13 23:50:56 +0530

    Assignment 16 Completed with no bugs and clean code -Lee

---
+++ Assignment_16/a5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+void displayPattern(int rows , int cols){
+	for(int i = 1 ; i <= rows ; ++i){
+		for(int j = 1 ; j <= cols ; ++j){
+			if(j == 1 || i == 1 || j == cols || i == rows){
+				cout<<j<<"\t";
+			}else{
+				cout <<"*\t";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int rows = 0  , cols = 0;
+	cout << "\nWelcome User "<<endl;
+	cout << "\nEnter number of rows : ";
+	cin >> rows; 
+	cout << "\nEnter numbe of cols : ";
+	cin >> cols;
+
+	displayPattern(rows ,cols);
+	return 0;
+}
